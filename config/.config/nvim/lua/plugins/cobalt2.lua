c683c457b3d8f15b4388e52360eb64f670b6e55e
eagle <feraleagle@protonmail.com>
2026-07-13 19:41:51 +0500

    Fixed Typos & Removed Some NVIM Plugins

--- config/.config/nvim/lua/plugins/cobalt2.lua
+++ config/.config/nvim/lua/plugins/cobalt2.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "lalitmee/cobalt2.nvim",
-    event = { "ColorSchemePre" }, -- ⚡ Official trigger for color plugins
-    dependencies = {
-      { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" }, -- 🔗 Pinning the version for stability
+    {
+        "lalitmee/cobalt2.nvim",
+        event = { "ColorSchemePre" }, -- ⚡ Official trigger for color plugins
+        dependencies = {
+            { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" }, -- 🔗 Pinning the version for stability
+        },
+        init = function()
+            -- The official way to call the colorscheme via the colorbuddy engine
+            require("colorbuddy").colorscheme("cobalt2")
+        end,
     },
-    init = function()
-      -- The official way to call the colorscheme via the colorbuddy engine
-      require("colorbuddy").colorscheme("cobalt2")
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "cobalt2",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "cobalt2",
+        },
     },
-  },
 }
