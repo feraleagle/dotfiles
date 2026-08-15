59495c9dfb6f9fffcadad858a6daa33135d1bf61
eagle <feraleagle@protonmail.com>
2026-08-15 23:46:53 +0500

    Nothing Special
    Quick Fixes

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -8,7 +8,3 @@ vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit Insert Mode" })
 -- Jump word backward/forward in Insert mode
 vim.keymap.set("i", "<C-l>", "<C-o>w", { noremap = true, silent = true }) -- jump forward one word
 vim.keymap.set("i", "<C-h>", "<C-o>b", { noremap = true, silent = true }) -- jump back one word
-
--- Jump to start/ end of line in Insert mode
-vim.keymap.set("i", "<C-a>", "<C-o>^", { desc = "Goto First Non-Blank Character In Insert Mode"})
-vim.keymap.set("i", "<C-e>", "<C-o>$", { desc = "Goto End of Line In Insert Mode"})
