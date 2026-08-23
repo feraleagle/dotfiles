04b235788dd6e5e32b5f07013f9d2193f85dd23b
eagle <feraleagle@protonmail.com>
2026-08-23 22:46:45 +0500

    Kitty-Diff Added

--- config/.config/hypr/modules/autostart.lua
+++ config/.config/hypr/modules/autostart.lua
@@ -8,9 +8,4 @@ hl.on("hyprland.start", function()
 
 	-- Start Noctalia v5 shell
 	hl.exec_cmd("noctalia")
-
-	-- Clipboard manager background workers (cliphist)
-	hl.exec_cmd("cliphist list | grep -E '\\[Binary data|image/' | cliphist delete")
-	hl.exec_cmd("wl-paste --type text --watch cliphist store")
-	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
